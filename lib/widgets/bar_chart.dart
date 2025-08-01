8d7728ccf9a73457d4ea5e3ac64b3d2fce8aa63b
Yasin-A10 <yasinzizi8383@gmail.com>
2025-08-01 18:52:55 +0330

    ...

--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -1,82 +1,3 @@
-// import 'package:fl_chart/fl_chart.dart';
-// import 'package:flutter/material.dart';
-// import 'package:safar_khaneh_panel/core/constants/colors.dart';
-// import 'package:safar_khaneh_panel/core/utils/number_formater.dart';
-
-// class BarChartWidget extends StatelessWidget {
-//   final List<int> reservations;
-//   final List<String> weekDays = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج'];
-
-//   BarChartWidget({super.key, required this.reservations});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AspectRatio(
-//       aspectRatio: 1.7,
-//       child: BarChart(
-//         BarChartData(
-//           alignment: BarChartAlignment.spaceAround,
-//           maxY: 100,
-//           barTouchData: BarTouchData(
-//             enabled: true,
-//             touchTooltipData: BarTouchTooltipData(
-//               getTooltipColor: (group) {
-//                 return AppColors.accentColor2;
-//               },
-//               getTooltipItem: (group, groupIndex, rod, rodIndex) {
-//                 return BarTooltipItem(
-//                   'تعداد رزرو: ${formatNumberToPersian(rod.toY.toInt())}',
-//                   const TextStyle(
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.bold,
-//                     fontSize: 14,
-//                   ),
-//                 );
-//               },
-//             ),
-//           ),
-//           titlesData: FlTitlesData(
-//             bottomTitles: AxisTitles(
-//               sideTitles: SideTitles(
-//                 showTitles: true,
-//                 getTitlesWidget: (value, meta) {
-//                   return Padding(
-//                     padding: const EdgeInsets.only(top: 6.0),
-//                     child: Text(
-//                       weekDays[value.toInt()],
-//                       style: const TextStyle(fontSize: 12),
-//                     ),
-//                   );
-//                 },
-//               ),
-//             ),
-//             leftTitles: AxisTitles(
-//               sideTitles: SideTitles(showTitles: true, reservedSize: 40),
-//             ),
-//             topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-//             rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-//           ),
-//           gridData: FlGridData(show: true),
-//           borderData: FlBorderData(show: false),
-//           barGroups: List.generate(reservations.length, (index) {
-//             return BarChartGroupData(
-//               x: index,
-//               barRods: [
-//                 BarChartRodData(
-//                   toY: reservations[index].toDouble(),
-//                   width: 20,
-//                   borderRadius: BorderRadius.circular(4),
-//                   color: Colors.indigo,
-//                 ),
-//               ],
-//             );
-//           }),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:safar_khaneh_panel/core/constants/colors.dart';
@@ -97,24 +18,22 @@ class BarChartWidget extends StatelessWidget {
 
   int getTodayIndex() {
     final now = DateTime.now();
-    final int gregorianWeekday = now.weekday; // شنبه: 6، یکشنبه: 7، ...
-    // جدول نگاشت به ترتیب ما:
-    // flutter: Mon=1 to Sun=7 → ما: ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج']
+    final int gregorianWeekday = now.weekday;
     switch (gregorianWeekday) {
       case 1:
-        return 1; // دوشنبه = د
+        return 1;
       case 2:
-        return 2; // سه‌شنبه = س
+        return 2;
       case 3:
-        return 3; // چهارشنبه = چ
+        return 3;
       case 4:
-        return 4; // پنج‌شنبه = پ
+        return 4;
       case 5:
-        return 5; // جمعه = ج
+        return 5;
       case 6:
-        return 6; // شنبه = ش
+        return 6;
       case 7:
-        return 0; // یکشنبه = ی
+        return 0;
       default:
         return 0;
     }
